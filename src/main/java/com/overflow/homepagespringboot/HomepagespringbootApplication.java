bee326d5c49becf3296060509b90cb2706aff722
OFCC-4K <overflowcheshirecat@gmail.com>
2023-07-17 14:59:00 +0900

    For Developing Homepage With Spring Boot.

---
+++ src/main/java/com/overflow/homepagespringboot/HomepagespringbootApplication.java
@@ -0,0 +1,12 @@
+package com.overflow.homepagespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomepagespringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomepagespringbootApplication.class, args);
+    }
+}
---
+++ src/test/java/com/overflow/homepagespringboot/HomepagespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.overflow.homepagespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomepagespringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
